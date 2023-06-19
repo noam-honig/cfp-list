1cccd5723076b19871ae627f7f1bf310ba19741f
Noam Honig <noam.honig@gmail.com>
2023-06-19 16:08:15 +0300

    #9

--- src/components/edit-cfp.tsx
+++ src/components/edit-cfp.tsx
@@ -3,11 +3,12 @@ import { FormEvent, useEffect, useState } from 'react'
 import { CFP } from '../shared/cfp'
 import { ErrorInfo, remult } from 'remult'
 import { useNavigate, useParams } from 'react-router'
-import '@vonage/vivid/text-field';
-import '@vonage/vivid/text-area';
-import '@vonage/vivid/button';
-import '@vonage/vivid/card';
-import '@vonage/vivid/layout';
+import '@vonage/vivid/text-field'
+import '@vonage/vivid/text-area'
+import '@vonage/vivid/button'
+import '@vonage/vivid/card'
+import '@vonage/vivid/layout'
+import { Link } from 'react-router-dom'
 
 const cfpRepo = remult.repo(CFP)
 
@@ -36,9 +37,8 @@ export default function EditCfp({ createNew }: { createNew: boolean }) {
 
   return (
     <vwc-card headline="Edit Conference">
-      
-        <form class="edit-cfp" slot="footer" onSubmit={save}>
-          <vwc-layout gutters="small" column-basis="medium">
+      <form class="edit-cfp" slot="footer" onSubmit={save}>
+        <vwc-layout gutters="small" column-basis="medium">
           {(
             [
               'conferenceName',
@@ -62,39 +62,45 @@ export default function EditCfp({ createNew }: { createNew: boolean }) {
 
             return (
               <div key={key}>
-                  {key == 'notes' ? (
-                    <vwc-text-area
-                      label="Notes"
-                      value={value}
-                      onBlur={(e) => setValue(e.target.value)}
-                    ></vwc-text-area>
-                  ) : (
-                    <>
+                {key == 'notes' ? (
+                  <vwc-text-area
+                    label="Notes"
+                    value={value}
+                    onBlur={(e) => setValue(e.target.value)}
+                  ></vwc-text-area>
+                ) : (
+                  <>
                     {meta.inputType === 'number' ? (
-                      <vwc-number-field label={meta.caption} 
-                                        value={value} 
-                                        onBlur={(e) => setValue(e.target.value)}>
-                      </vwc-number-field>
+                      <vwc-number-field
+                        label={meta.caption}
+                        value={value}
+                        onBlur={(e) => setValue(e.target.value)}
+                      ></vwc-number-field>
                     ) : (
-                      <vwc-text-field type={meta.inputType}
-                                      label={meta.caption} 
-                                      value={value} 
-                                      onBlur={(e) => setValue(e.target.value)}>
-                      </vwc-text-field>
+                      <vwc-text-field
+                        type={meta.inputType}
+                        label={meta.caption}
+                        value={value}
+                        onBlur={(e) => setValue(e.target.value)}
+                      ></vwc-text-field>
                     )}
-                    </>
-                  )}
+                  </>
+                )}
                 {error ?? <div>{error}</div>}
               </div>
             )
           })}
-          </vwc-layout>
-          <vwc-button label="Save" 
-                      type="submit" 
-                      appearance="filled" 
-                      connotation="success"></vwc-button>
-        </form>
-
+        </vwc-layout>
+        <vwc-button
+          label="Save"
+          type="submit"
+          appearance="filled"
+          connotation="success"
+        ></vwc-button>
+        <Link to="/">
+          <vwc-button label="Cancel" appearance="filled"></vwc-button>
+        </Link>
+      </form>
     </vwc-card>
   )
 }
