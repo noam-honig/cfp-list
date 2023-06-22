27cc1f63fb891c6eeeaecedef58ee4ac1be6cda9
Noam Honig <noam.honig@gmail.com>
2023-06-22 20:41:19 +0300

    fixed minor error

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), express('src/server')],
   build: {
     rollupOptions: {
-      external: ["node-fetch"],
+      external: ['node-fetch', 'open-graph-scraper'],
     },
   },
 })
